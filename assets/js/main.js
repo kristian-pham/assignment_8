a38a581168a5db3491234659c2cb132874c015b3
kristian-pham <91481232+kristian-pham@users.noreply.github.com>
2021-12-03 15:34:15 -0500

    Finished code and deleted extra files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar-fixed-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
+// Execute something when DOM is ready:
+$(document).ready(function(){
+   // Delay the action by 10000ms
+   setTimeout(function(){
+      // Display the div containing the class "bottomdiv"
+      $(".hide").show();
+   }, 10000);
 });
